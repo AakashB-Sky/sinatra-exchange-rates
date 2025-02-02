e253be0d5715f22f1a682a2fca26bfb1fc995f9d
AakashB-Sky <62520487+AakashB-Sky@users.noreply.github.com>
2025-02-02 20:48:23 +0000

    figured out API call in sandbox

--- sandbox.rb
+++ sandbox.rb
@@ -16,4 +16,20 @@ currency_list_hash = parsed_response.fetch("currencies") # isolate the currency 
 # pp currency_list_hash
 
 currency_list_array = currency_list_hash.keys # put the three-letter currency codes into an array
-pp currency_list_array
+# pp currency_list_array
+
+# documentation on converting one currency to another (https://exchangerate.host/documentation) 
+# https://api.exchangerate.host/convert?from=EUR&to=GBP&amount=100
+
+from_currency = "USD"
+to_currency = "EUR"
+amount = 1
+conversion_url = "https://api.exchangerate.host/convert?from=#{from_currency}&to=#{to_currency}&amount=#{amount}&access_key=#{ENV.fetch("EXCHANGE_RATE_KEY")}"
+  # order of query string doesn't seem to matter
+
+raw_FX_response = HTTP.get(conversion_url)
+# pp raw_FX_response
+parsed_FX_response = JSON.parse(raw_FX_response)
+# pp parsed_FX_response
+exchange_rate = parsed_FX_response.fetch("result")
+pp exchange_rate
